37583acee9bdb14025568b28eb4097652c0cb11f
Ic4ro-y <igor_walchholz@estudante.sesisenai.org.br>
2025-09-11 18:43:30 -0300

    feito

---
+++ 3- exercicio pratico.js
@@ -0,0 +1,8 @@
+let meuNome = "Igor"
+let minhaIdade = 16
+let minhacidade = "pomerode - SC"
+let gostodosmeusgatos = "Tom e BIBI"
+
+console.log("Meu nome é " + meuNome + ", tenho " + minhaIdade + " anos.")
+console.log("Moro em " + minhacidade)
+console.log("Tenho dois cachorros, chamados " + gostodosmeuscachorros)
---
+++ 4- condicionais.js
@@ -0,0 +1,10 @@
+
+let nota = 7.0
+
+if (nota >= 7.0 && nota <= 10.0) {
+    console.log("Aprovado")
+} else {
+    console.log("Reprovado")
+}
+
+console.log(typeof nota)
